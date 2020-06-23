21830c454e639ba1377840f1f4cd61c5426eb997
axstav <axstav94@gmail.com>
2020-06-23 00:49:18 -0500

    4

--- wp-config.php
+++ wp-config.php
@@ -81,6 +81,12 @@ define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
 
 /* That's all, stop editing! Happy publishing. */
+define('FORCE_SSL_ADMIN', true);
+// in some setups HTTP_X_FORWARDED_PROTO might contain 
+// a comma-separated list e.g. http,https
+// so check for https existence
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+       $_SERVER['HTTPS']='on';
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
